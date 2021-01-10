0422ff7691e0c923524cb46d2b59671b40cbe6ed
Merritt <9575627+Merrit@users.noreply.github.com>
2021-01-10 15:05:07 -0500

    Implement auto-capitalize new item names

--- lib/list/add_list_item.dart
+++ lib/list/add_list_item.dart
@@ -4,12 +4,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 import 'package:shopping_list/firestore/firestore_user.dart';
+import 'package:shopping_list/helpers/capitalize_string.dart';
 
 /// Add a new item to the current shopping list.
 void addListItem(
     {@required BuildContext context, @required String itemName, String aisle}) {
   var _aisle = aisle ?? 'Unsorted';
   FirestoreUser user = Provider.of<FirestoreUser>(context, listen: false);
+  itemName = itemName.capitalizeFirst;
 
   FirebaseFirestore.instance.collection('lists').doc(user.currentList).set(
     {
