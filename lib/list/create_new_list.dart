085f10cfcbecb3436e4bb95b10eb73487103d2cc
Merritt <9575627+Merrit@users.noreply.github.com>
2021-01-10 12:34:58 -0500

    Refactor create_new_list

--- lib/list/create_new_list.dart
+++ lib/list/create_new_list.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
+
 import 'package:shopping_list/firestore/firestore_user.dart';
 import 'package:shopping_list/globals.dart';
 
