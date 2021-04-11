96de161311d394e617f31882a83027905d4d858d
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-04-11 12:29:07 -0400

    Add auth facade

---
+++ lib/domain/auth/auth_failure.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'auth_failure.freezed.dart';
+
+@freezed
+abstract class AuthFailure with _$AuthFailure {
+  const factory AuthFailure.cancelledByUser() = CancelledByUser;
+  const factory AuthFailure.emailAlreadyInUse() = EmailAlreadyInUse;
+  const factory AuthFailure.invalidEmailAndPasswordCombination() =
+      InvalidEmailAndPasswordCombination;
+  // Catch-all failure if we don't know what went wrong.
+  const factory AuthFailure.serverError() = ServerError;
+}
---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:shopping_list/domain/auth/auth_failure.dart';
+import 'package:shopping_list/domain/auth/value_objects.dart';
+
+abstract class IAuthFacace {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
