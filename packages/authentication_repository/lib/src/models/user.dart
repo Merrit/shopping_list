7a7a79f21988124c1a198229719ef612a2e856c0
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-04-16 15:57:42 -0400

    Move user model to own file

---
+++ packages/authentication_repository/lib/src/models/user.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+/// User model.
+///
+/// [User.empty] represents an unauthenticated user.
+class User extends Equatable {
+  final String id;
+  final String email;
+  final bool emailIsVerified;
+
+  const User({
+    required this.id,
+    required this.email,
+    required this.emailIsVerified,
+  });
+
+  static const empty = User(
+    email: '',
+    id: '',
+    emailIsVerified: false,
+  );
+
+  @override
+  List<Object> get props => [id, email, emailIsVerified];
+}
