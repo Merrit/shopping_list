dd2effcd966f10471bc50ccfd639a5c35acc507f
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-04-17 11:36:50 -0400

    Add tests for Email class

---
+++ test/authentication/models/email_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shopping_list/authentication/models/email.dart';
+
+void main() {
+  test('Can instantiate initial', () {
+    final email = Email.initial();
+    expect(email, isA<Email>());
+    expect(email.value, '');
+    expect(email.isValid, isFalse);
+  });
+
+  test('Valid email is valid', () {
+    final email = Email('Iroh@BaSing.Se');
+    expect(email.value, 'Iroh@BaSing.Se');
+    expect(email.isValid, isTrue);
+  });
+
+  test('Invalid email is invalid', () {
+    final email = Email('lemur');
+    expect(email.isValid, isFalse);
+  });
+}
