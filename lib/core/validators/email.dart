72f38080ec9d1ef78bed23c55ecfc13107354474
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-04-17 13:12:56 -0400

    Add doc comment to email  validator

--- lib/core/validators/email.dart
+++ lib/core/validators/email.dart
@@ -1,3 +1,9 @@
+/// A valid email address should contain an at sign: @.
+///
+/// This is generally considered a good method of validating an
+/// email field given the extreme possibilities that are now
+/// possible with domains and addresses - especially when
+/// paired with a verification email sent to the user.
 bool validateEmailAddress(String value) {
   return (value.contains('@')) ? true : false;
 }
