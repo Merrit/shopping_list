bd17cbe6538db05928c1a92278ec56d61619e8c0
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-05-16 14:58:47 -0400

    Add SimpleTooltip widget

---
+++ lib/core/widgets/simple_tooltip.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+/// Display a tooltip with a single tap or click,
+/// where the default widget requires a long press / long click.
+class SimpleTooltip extends StatelessWidget {
+  final String message;
+  final Widget child;
+
+  SimpleTooltip({
+    Key? key,
+    required this.message,
+    required this.child,
+  }) : super(key: key);
+
+  final _toolTipKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final dynamic _toolTip = _toolTipKey.currentState;
+        _toolTip.ensureTooltipVisible();
+      },
+      child: Tooltip(
+        key: _toolTipKey,
+        message: message,
+        child: child,
+      ),
+    );
+  }
+}
