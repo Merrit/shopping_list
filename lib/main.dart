25332c6c26fa53a598e1768b1d8e268e628545a4
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-05-19 10:23:50 -0400

    Cache fonts
    Fixes jank when loading emoji on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
@@ -13,12 +15,22 @@ import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  primeFonts();
   await Firebase.initializeApp();
   EquatableConfig.stringify = kDebugMode;
   initLogger();
   // Bloc.observer = SimpleBlocObserver();
   setup.init();
   runApp(App(authenticationRepository: AuthenticationRepository()));
+}
+
+/// Pre-cache the fonts so there is no jank loading emoji on web.
+///
+/// https://github.com/flutter/flutter/issues/42586#issuecomment-541870382
+void primeFonts() {
+  final pb = ParagraphBuilder(ParagraphStyle(locale: window.locale));
+  pb.addText('\ud83d\ude01'); // Smiley-face emoji.
+  pb.build().layout(ParagraphConstraints(width: 100));
 }
 
 /// Initialize the logger.
