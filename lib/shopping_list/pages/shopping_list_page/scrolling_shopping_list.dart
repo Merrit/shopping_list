8a09cc80f3500866ee1412f0ae9e72847a079ab5
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-05-24 16:34:44 -0400

    Tweak scrollbar on desktop

--- lib/shopping_list/pages/shopping_list_page/scrolling_shopping_list.dart
+++ lib/shopping_list/pages/shopping_list_page/scrolling_shopping_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_list/core/core.dart';
+import 'package:shopping_list/core/helpers/form_factor.dart';
 
 import '../../shopping_list.dart';
 
@@ -18,30 +20,36 @@ class ScrollingShoppingList extends StatelessWidget {
       });
     });
 
+    final _isLargeFormFactor = isLargeFormFactor(context);
+
     return BlocBuilder<ShoppingListCubit, ShoppingListState>(
       builder: (context, state) {
         final items = state.items.where((e) => e.isComplete == false).toList();
 
-        return ListView.separated(
+        return Scrollbar(
           controller: _scrollController,
-          padding: const EdgeInsets.only(
-            left: 4,
-            right: 4,
-            top: 4,
-            bottom: 100,
+          isAlwaysShown: _isLargeFormFactor ? true : false,
+          child: ListView.separated(
+            controller: _scrollController,
+            padding: const EdgeInsets.only(
+              left: 4,
+              right: 4,
+              top: 4,
+              bottom: 100,
+            ),
+            separatorBuilder: (context, index) {
+              return const Divider(
+                indent: 20,
+                endIndent: 20,
+                height: 4,
+              );
+            },
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              var item = items[index];
+              return ItemTile(item: item);
+            },
           ),
-          separatorBuilder: (context, index) {
-            return const Divider(
-              indent: 20,
-              endIndent: 20,
-              height: 4,
-            );
-          },
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            var item = items[index];
-            return ItemTile(item: item);
-          },
         );
       },
     );
